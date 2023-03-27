7d4445b71de63e984ca871558999579a2b410a35
Jenniferlyx <lyuxiao2000@gmail.com>
2023-03-27 18:13:06 -0400

    copy download script from mtg

---
+++ download_audio.sh
@@ -0,0 +1,8 @@
+mkdir -p data/raw_data
+python3 data/download/download.py \
+        --dataset raw_30s \
+        --type audio-low \
+        --num_dir 10 \
+        data/raw_data \
+        --unpack \
+        --remove
