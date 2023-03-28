6258e7669794326ed198151e3b4b17794207359a
Jenniferlyx <lyuxiao2000@gmail.com>
2023-03-28 11:38:28 -0400

    modify env

--- data/download_audio.sh
+++
@@ -1,8 +0,0 @@
-mkdir data/raw_data
-python3 ../mtg-jamendo-dataset/scripts/download/download.py \
-        --dataset raw_30s \
-        --type audio-low \
-        --num_dir 10 \
-        data/raw_data \
-        --unpack \
-        --remove
